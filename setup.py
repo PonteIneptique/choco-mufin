41eedc89f2e35cf1dda91a892270400e23a8b511
Thibault Clérice <leponteineptique@gmail.com>
2022-04-07 11:09:12 +0200

    Updated setup with the current form of CLI

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ DESCRIPTION = '[CHaracter Ocr COordination for MUFI iN texts] ' \
 URL = 'https://github.com/ponteineptique/choco-mufin'
 AUTHOR = 'Thibault Clerice'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.0.4"
+VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
 
@@ -107,7 +107,7 @@ setup(
     packages=["chocomufin"],
 
     entry_points={
-        'console_scripts': ['chocomufin=chocomufin:main_wrap'],
+        'console_scripts': ['chocomufin=chocomufin:cli.main'],
     },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
@@ -119,7 +119,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Text Processing :: Linguistic'
