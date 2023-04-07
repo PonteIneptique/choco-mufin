b1c009cde3d2969080045ca93d77d0447ffdbe9d
Thibault Clérice <leponteineptique@gmail.com>
2023-04-07 15:41:03 +0200

    Looser def

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ DESCRIPTION = '[CHaracter Ocr COordination for MUFI iN texts] ' \
 URL = 'https://github.com/ponteineptique/choco-mufin'
 AUTHOR = 'Thibault Clerice'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.1.10"
+VERSION = "0.1.11"
 
 # What packages are required for this module to be executed?
 
